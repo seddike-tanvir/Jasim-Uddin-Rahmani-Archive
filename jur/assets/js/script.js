eb13de3ebcf185138fc3d1f5fd5095b6085ed1d1
sm <contact.smtanvir@gmail.com>
2024-02-04 14:15:31 +0600

    Commited

--- jur/assets/js/script.js
+++ jur/assets/js/script.js
@@ -1,6 +1,11 @@
-// let btn = document.querySelector(".ply_btn");
+// // let btn = document.querySelector(".ply_btn");
 // let frame = document.querySelector(".player");
 
 
 
+// let insideIframeVdoTag = window.frames['frame'].contentDocument.getElementByagName('video');
+//     insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
+//     insideIframeVdoTag.setAttribute("nodownload", "");
+//     // insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
+//     // insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
 
