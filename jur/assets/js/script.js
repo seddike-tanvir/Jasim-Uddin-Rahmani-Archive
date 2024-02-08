eff47e75c61182a3345b44c0e2a050cfc9ed2d41
sm <contact.smtanvir@gmail.com>
2024-02-09 04:44:33 +0600

    Commited

--- jur/assets/js/script.js
+++ jur/assets/js/script.js
@@ -1,11 +1,24 @@
-// // let btn = document.querySelector(".ply_btn");
-// let frame = document.querySelector(".player");
+// // // let btn = document.querySelector(".ply_btn");
+// let frame = document.querySelectorAll('.player_frm');
+// let player = document.querySelectorAll('.player');
+// let audioSrc = document.querySelectorAll('.audio_src').src;
+// // let link = audioSrc.getAttribute("href").value;
+// // document.getElementsByClassName("audio_src").src;
+// // let frame = document.querySelectorAll(".player");
 
 
 
-// let insideIframeVdoTag = window.frames['frame'].contentDocument.getElementByagName('video');
-//     insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
-//     insideIframeVdoTag.setAttribute("nodownload", "");
-//     // insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
-//     // insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
+// // let insideIframeVdoTag = window.frames['frame'].contentDocument.getElementByagName('video');
+// //     insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
+// //     insideIframeVdoTag.setAttribute("nodownload", "");
+// //     // insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
+// //     // insideIframeVdoTag.setAttribute("disablepictureinpicture", "");
+
+
+// function plyAudio() {
+//     // player.src = audioSrc;
+//     player.src = 'Alo Alo .mp3';
+//     console.log('done');
+// }
+
 
